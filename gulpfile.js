09215510b994eceaf00642b445e80e83155fe403
bdwain <bryandwain@gmail.com>
2015-03-10 01:16:47 -0500

    prevent clean from running in parallel with build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -16,11 +16,13 @@ gulp.task('clean', 'Clean assets and dist', function (cb) {
   del([global.asset_path, global.dist_path], cb);
 });
 
-gulp.task('default', 'Start a server and run js/css build watchers and test watchers', 
-  ['clean', 'js:watch', 'css:watch', 'server'], null, {aliases: ['run', 'start']});
+gulp.task('default', 'Start a server and run js/css build watchers and test watchers', ['clean'], function(cb){
+  runSequence(['js:watch', 'css:watch', 'server'], cb); //these just need to run after. they can be in parallel
+}, {aliases: ['run', 'start']});
 
-gulp.task('debug', 'Same as the default task, but uses chrome as the test browser so you can use the js debugger', 
-  ['clean', 'js:watch:debug', 'css:watch', 'server']);
+gulp.task('debug', 'Same as the default task, but uses chrome as the test browser so you can use the js debugger', ['clean'], function(cb){
+  runSequence(['js:watch:debug', 'css:watch', 'server'], cb); //these just need to run after. they can be in parallel
+});
 
 gulp.task('test', 'Run all javascript tests', ['js:test']);
 
